a98ae89c7c68ae8e019d43ed7186b508994b7fb7
Eddy Martinez <eddymartinezsaldana@gmail.com>
2021-06-26 09:57:30 +0100

    Fixes bug of crashing when alerts popup appears in PrimeLocation.com.

--- url_finders/prime_location_url_finder.py
+++ url_finders/prime_location_url_finder.py
@@ -1,12 +1,8 @@
-import time
-from typing import List
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from pprint import pprint
-#from cities import get_cities
 from url_finders.url_finder import UrlFinder
 
 class PrimeLocationUrlFinder(UrlFinder):
@@ -19,7 +15,7 @@ class PrimeLocationUrlFinder(UrlFinder):
         # PrimeLocation driver
         pl_driver = webdriver.Chrome()
         url_to_scrape="https://www.primelocation.com/"
-        print(f"\nFetching url{url_to_scrape}\n")
+        print(f"\nFetching url {url_to_scrape}\n")
         pl_driver.get(url_to_scrape)
         wait = WebDriverWait(pl_driver, 10)
         element = wait.until(EC.visibility_of_element_located((By.NAME, "search-form")))
@@ -47,6 +43,10 @@ class PrimeLocationUrlFinder(UrlFinder):
 
         areas_of_UK = ["London", "South East England", "East Midlands", "East of England", "North East England", "North West England", "South West England", "West Midlands", "Yorkshire and The Humber", "Isle of Man", "Channel Isles", "Scotland", "Wales", "Northern Ireland"]
 
+        # Quitting the cookies banner
+        pl_driver.find_element_by_xpath(
+            "//button[@title='Agree to our use of cookies']").click()
+
         # Selecting the "To rent" option
         pl_driver.find_element_by_xpath("//option[@value='to-rent']").click()
 
@@ -58,23 +58,15 @@ class PrimeLocationUrlFinder(UrlFinder):
         # Selecting the 50 entries per page (maximum)
         Select(pl_driver.find_element_by_xpath(
             "//select[@name='page_size']")).select_by_value(value='50')
-        num_links = 50
 
         to_rent_links = []
-        for area in areas_of_UK:
-            # Search each area
+        for area in areas_of_UK: # Search each area
             search_next = pl_driver.find_element_by_xpath("//input[@id='location']")
             search_next.clear()
             search_next.send_keys(f"{area}")
             click_search()
-            print(f"Searching for city {area}")
+            print(f"Searching for region of {area}")
 
-            # Getting featured properties links
-            featured_links = pl_driver.find_elements_by_xpath("//ul[@id='featured_listings']/li//div[@class='status-wrapper']/a")
-            for link in featured_links:
-                to_rent_links.append(link.get_attribute("href"))
-
-            # Each city
             # While there are pages left
             while True:
                 # Getting all the links
@@ -82,7 +74,7 @@ class PrimeLocationUrlFinder(UrlFinder):
                 for link in links:
                     to_rent_links.append(link.get_attribute("href"))
                     #print(link.get_attribute("href"))
-                print(f"Scraped first {num_links}  links")
+                print(f"Scraped first {len(to_rent_links)} links")
 
                 # Going to next search page
                 search_pages = more_pages()
@@ -92,6 +84,13 @@ class PrimeLocationUrlFinder(UrlFinder):
 /a[contains(text(),'{search_pages[0]}')]").click()
                 else:
                     break
+
+                # Quitting alerts popup
+                try:
+                    pl_driver.find_element_by_xpath(
+                        "//div[@class='alerts-popup-wrapper']//a[@href='#']").click()
+                except Exception:
+                    pass
                 print(f"Scraping page: {search_pages[0]}")
-                num_links += 50
+
         self.save_urls_to_db(to_rent_links)
