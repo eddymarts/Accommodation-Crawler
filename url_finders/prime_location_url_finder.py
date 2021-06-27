d1f4d4acc4dce437bd16698fbb75099e1e4bd882
Eddy Martinez <eddymartinezsaldana@gmail.com>
2021-06-27 03:14:16 +0100

    Completes PrimeLocation URL Finder and optimises code.

--- url_finders/prime_location_url_finder.py
+++ url_finders/prime_location_url_finder.py
@@ -1,96 +1,61 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 from url_finders.url_finder import UrlFinder
 
 class PrimeLocationUrlFinder(UrlFinder):
     def __init__(self, db_session) -> None:
         self.parser_to_use = "PrimeLocation"
         super().__init__(db_session)
+        self.url_to_scrape = "https://www.primelocation.com/"
+    
+    def get_links(self, option, region) -> None:
+        """ Gets all property links within a region. """
+        page = 1
 
-    # Return all the URLs we want to scrape
-    def find(self):
-        # PrimeLocation driver
-        pl_driver = webdriver.Chrome()
-        url_to_scrape="https://www.primelocation.com/"
-        print(f"\nFetching url {url_to_scrape}\n")
-        pl_driver.get(url_to_scrape)
-        wait = WebDriverWait(pl_driver, 10)
-        element = wait.until(EC.visibility_of_element_located((By.NAME, "search-form")))
+        while True:
+            # Open new tab
+            self.driver.find_element_by_tag_name(
+                'body').send_keys(Keys.CONTROL + 't') 
+            self.driver.get(
+        self.url_to_scrape + f"{option}/property/{region}/?\
+page_size=50&search_source=refine&radius=0&view_type=grid&pn={page}")
 
-        def more_pages() -> bool:
-            """
-            Checks if there are more pages to scrape.
-
-            INPUT: None
-            OUTPUT: Dictionary with
-                    0: Next page to scrape.
-                    1: True if there are more pages, False otherwise.
-            """
-            next_page = str(int(pl_driver.find_element_by_xpath(
-                "//div[@class='paginate bg-muted']/span[@class='current']").text) + 1)
-            pages = pl_driver.find_elements_by_xpath("//div[@class='paginate bg-muted']/a")
-
-            return {
-                0: next_page,
-                1: next_page in [page.text for page in pages]}
-
-        def click_search():
-            """ Clicks the search button """
-            pl_driver.find_element_by_xpath("//button[@value='Search']").click()
-
-        areas_of_UK = ["London", "South East England", "East Midlands", "East of England", "North East England", "North West England", "South West England", "West Midlands", "Yorkshire and The Humber", "Isle of Man", "Channel Isles", "Scotland", "Wales", "Northern Ireland"]
-
-        # Quitting the cookies banner
-        pl_driver.find_element_by_xpath(
-            "//button[@title='Agree to our use of cookies']").click()
-
-        # Selecting the "To rent" option
-        pl_driver.find_element_by_xpath("//option[@value='to-rent']").click()
-
-        # Entering the city
-        pl_driver.find_element_by_xpath(
-            "//input[@id='search-input-location']").send_keys(f"{areas_of_UK[-1]}")
-        click_search()
-
-        # Selecting the 50 entries per page (maximum)
-        Select(pl_driver.find_element_by_xpath(
-            "//select[@name='page_size']")).select_by_value(value='50')
-
-        to_rent_links = []
-        for area in areas_of_UK: # Search each area
-            search_next = pl_driver.find_element_by_xpath("//input[@id='location']")
-            search_next.clear()
-            search_next.send_keys(f"{area}")
-            click_search()
-            print(f"Searching for region of {area}")
-
-            # While there are pages left
-            while True:
-                # Getting all the links
-                links = pl_driver.find_elements_by_xpath("//div[@class='listing-results-wrapper']//a[@class='photo-hover']")
+            links = self.driver.find_elements_by_xpath(
+"//div[@class='listing-results-wrapper']//a[@class='photo-hover']")
+            if links:
                 for link in links:
-                    to_rent_links.append(link.get_attribute("href"))
+                    self.property_links.append(link.get_attribute("href"))
                     #print(link.get_attribute("href"))
-                print(f"Scraped first {len(to_rent_links)} links")
+                print(f"Page {page}: Scraped first {len(self.property_links)} links.")
+                page += 1
+            else: # End of search
+                # Close tab
+                self.driver.find_element_by_tag_name(
+                    'body').send_keys(Keys.CONTROL + 'w')
+                break
 
-                # Going to next search page
-                search_pages = more_pages()
-                if search_pages[1]:
-                    pl_driver.find_element_by_xpath(
-                f"//div[@class='paginate bg-muted']\
-/a[contains(text(),'{search_pages[0]}')]").click()
-                else:
-                    break
 
-                # Quitting alerts popup
-                try:
-                    pl_driver.find_element_by_xpath(
-                        "//div[@class='alerts-popup-wrapper']//a[@href='#']").click()
-                except Exception:
-                    pass
-                print(f"Scraping page: {search_pages[0]}")
+    def find(self) -> None:
+        """
+        Finds all property links in PrimeLocation.com and saves it to local database.
+        """
+        self.property_links = []
 
-        self.save_urls_to_db(to_rent_links)
+        # Open browser
+        self.driver = webdriver.Chrome()
+        areas_of_UK = ["London", "South East England", "East Midlands", "East of England", "North East England", "North West England", "South West England", "West Midlands", "Yorkshire and The Humber", "Isle of Man", "Channel Isles", "Scotland", "Wales", "Northern Ireland"]
+        regions = [region.replace(" ", "-") for region in areas_of_UK]
+
+        for option in ["for-sale", "to-rent"]:
+            print(f"\nFetching {option} properties in {self.url_to_scrape}\n")
+
+            for region in regions:
+                page = 1
+                print(f"\nFetching properties in {region}\n")
+                self.get_links(option, region)
+
+        # Close browser
+        self.driver.close()
+
+        # Save to database
+        self.save_urls_to_db(self.property_links)
