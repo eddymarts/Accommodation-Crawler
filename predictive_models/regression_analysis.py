1bec6d26f3e37400fa340d12eb8ec89b39d776fe
Eddy Martínez <emartinez@restrata.com>
2021-08-20 08:28:37 +0100

    Renames regression.py to regression_analysis.py.

--- predictive_models/regression_analysis.py
+++ predictive_models/regression_analysis.py
@@ -1,34 +1,9 @@
 import pandas as pd
 from pprint import pprint
 from scale_split import scalesplit
-from models.models import *
-from models.base import ModelSelector
+from machine_learning.regression_models import *
+from machine_learning.base import ModelSelector
 from get_data import Data
-
-def show(df, max_rows=6, max_columns=None):
-    """ Prints all columns of dataframe. """
-
-    with pd.option_context('display.max_rows', max_rows,
-                            'display.max_columns', max_columns):
-        pprint(df)
-
-def describe(df, max_columns=None):
-    """ Prints describe() of all columns of dataframe. """
-
-    with pd.option_context('display.max_columns', max_columns):
-        print(df.describe())
-
-def info(df):
-    """ Prints dataframe.info() """
-
-    print(df.info())
-
-def analyse(df):
-    """ Prints relevant information about the dataframe. """
-
-    show(df)
-    describe(df)
-    info(df)
 
 properties = Data()
 
