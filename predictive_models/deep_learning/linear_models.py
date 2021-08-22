98712d64182a6f564d39e1ec089bc68480e764b5
Eddy Martinez <eddymartinezsaldana@gmail.com>
2021-08-22 15:36:59 +0100

    Deletes redundant methods that will be overwritten by Skorch methods.

--- predictive_models/deep_learning/linear_models.py
+++ predictive_models/deep_learning/linear_models.py
@@ -14,17 +14,6 @@ class LinearRegression(BaseNetwork):
     def __init__(self, n_features, n_labels):
         super().__init__()
         self.layers = torch.nn.Linear(n_features, n_labels)
-    
-    def get_loss(self, y_hat, y):
-        """
-        Gets Mean Squared Error between predictions of X and actual value (y).
-        
-        INPUT:  y_hat -> Tensor with predicted values.
-                y -> Tensor with labels.
-        
-        OUTPUT: loss -> Mean Squared Error between predictions and actual labels.
-        """
-        return F.mse_loss(y_hat, y)
 
 class BinaryLogisticRegression(LinearRegression):
     """
@@ -39,19 +28,6 @@ class BinaryLogisticRegression(LinearRegression):
         self.layers = torch.nn.Sequential(
             torch.nn.Linear(n_features, n_labels), 
             torch.nn.Sigmoid())
-    
-    def __call__(self, X):
-        """
-        Predicts the value of an output for each row of X
-        using the Binary Logistic Regression model.
-        """
-        return torch.round(self.forward(X))
-    
-    def get_loss(self, y_hat, y):
-        """
-        Gets Binary Cross Entropy between predictions (y_hat) and actual value (y).
-        """
-        return F.binary_cross_entropy(y_hat, y)
 
 class LogisticRegression(BinaryLogisticRegression):
     """
@@ -66,16 +42,3 @@ class LogisticRegression(BinaryLogisticRegression):
         self.layers = torch.nn.Sequential(
             torch.nn.Linear(n_features, n_labels), 
             torch.nn.Softmax(1))
-    
-    def __call__(self, X):
-        """
-        Predicts the value of an output for each row of X
-        using the Logistic Regression model.
-        """
-        return torch.argmax(self.forward(X), axis=1).reshape(-1, 1)
-    
-    def get_loss(self, y_hat, y):
-        """
-        Gets Cross Entropy between predictions (y_hat) and actual value (y).
-        """
-        return F.cross_entropy(y_hat, y.long().reshape(-1))
