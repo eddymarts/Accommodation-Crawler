a6b0ed3e9bd3753e409423f1991ab727ffb0bd0d
Eddy Martínez <eddymartinezsaldana@gmail.com>
2021-08-31 23:56:24 +0100

    Uses ModelSelector class only for evaluating models that are previously generated.

--- predictive_analysis/models/base.py
+++ predictive_analysis/models/base.py
@@ -3,6 +3,7 @@ from sklearn.metrics import *
 import time
 import torch
 import numpy as np
+import pickle
 
 class BaseModel:
     def __init__(self, X, y) -> None:
@@ -17,12 +18,16 @@ class BaseModel:
         grid_search.fit(X=self.X, y=self.y)
         tuning_end = time.time()
 
-        self.tuning_time = tuning_end - tuning_start
+        self.tune_time = tuning_end - tuning_start
         self.best_model = grid_search.best_estimator_
         self.best_hyperparameters = grid_search.best_params_
         self.fitting_time = grid_search.refit_time_
+        self.results = grid_search.cv_results_['params'][grid_search.best_index_]
+        self.results['score'] = grid_search.best_score_
+        self.results['fit_time'] = grid_search.refit_time_
+        self.results['tune_time'] = self.tune_time
 
-    def score(self, X_sets, y_sets, labels):
+    def score(self, X_sets, y_sets):
         """ Returns the score of the tuned model for every set of the data. """
         self.scores = {"accuracy": {}, "f1": {}, "log_loss": {}}
         y_pred_sets = {}
@@ -36,14 +41,14 @@ class BaseModel:
             # self.scores["cross_entropy"][set] = log_loss(y_sets[set], y_pred_sets[set], labels=labels)
 
 class ModelSelector:
-    def __init__(self, models, X, y) -> None:
-        self.models = {model.__name__: model(X, y) for model in models}
+    def __init__(self, models) -> None:
+        self.models = models
 
     
-    def get_best_model(self, X_sets, y_sets, labels):
+    def get_best_model(self, X_sets, y_sets):
         max_score = 0
         for key in self.models.keys():
-            self.models[key].score(X_sets, y_sets, labels)
+            self.models[key].score(X_sets, y_sets)
 
             if self.models[key].scores["accuracy"][1] > max_score:
                 max_score = self.models[key].scores["accuracy"][1]
